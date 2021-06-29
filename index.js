8ea7669ed2e02983121cae4a744d09db19133ca1
nhuse <nathan.huse@yahoo.com>
2021-06-28 20:06:21 -0500

    commit tree bd99d1bc097fe2e2e2c083bf6e9c46057c695c11

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    const foo = arr => {
+        let newArr = [];
+        newArr.push(arr[0]);
+        newArr.push(arr[1]);
+        return newArr;
+    };
+    return foo(drivers);
+}
+
+function returnLastTwoDrivers(drivers) {
+    const foo = arr => {
+        let newArr = [];
+        newArr.push(arr[arr.length-2]);
+        newArr.push(arr[arr.length-1]);
+        return newArr;
+    };
+    return foo(drivers);
+}
+
+let selectingDrivers = [];
+selectingDrivers.push(returnFirstTwoDrivers);
+selectingDrivers.push(returnLastTwoDrivers);
+
+function createFareMultiplier(num) {
+    return function(fare) {
+        return fare * num;
+    }
+}
+
+function fareDoubler(fare) {
+    const totalFare = () => {
+        return createFareMultiplier(2)(fare);
+    }
+    return totalFare();
+}
+
+function fareTripler(fare) {
+    const totalFare = () => {
+        return createFareMultiplier(3)(fare);
+    }
+    return totalFare();
+}
+
+function selectDifferentDrivers(arrayOfDrivers, returnFirstOrLast) {
+    if(returnFirstOrLast === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(arrayOfDrivers);
+    }
+    else {
+        return returnLastTwoDrivers(arrayOfDrivers);
+    }
+}
